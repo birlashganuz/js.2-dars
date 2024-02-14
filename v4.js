907a7a22616aaf81bc351089e81f495e64e926a5
birlashganuz <birlashganuz@gmail.com>
2024-02-14 18:19:03 +0500

    Boshqa

---
+++ v4.js
@@ -0,0 +1,8 @@
+a = +prompt('Ixtiyoriy natural son kiriting: ')
+if (a != 0) {
+    if (a % 2 == 0) {
+        alert(a + " juft son")
+    } else {
+        alert(a + " toq son")
+    }
+} else { alert( a + " juft ham toq ham emas") }
---
+++ v5.js
@@ -0,0 +1,12 @@
+a = +prompt('1-sonni kiriting: ')
+b = +prompt('1-sonni kiriting: ')
+c = +prompt('1-sonni kiriting: ')
+max = a
+if (max <= b) {
+    max = b
+}
+if (max <= c) {
+    max = c
+}
+
+alert('Berilgan sonlar ichida eng kattasi: ' + max)
